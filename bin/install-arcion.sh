91389dcdb139c1c5cb18f0432f9da0bc91b48ab8
Robert Lee <sangkyulee@gmail.com>
2024-02-01 10:23:05 -0500

    arcion: install 24.01

--- bin/install-arcion.sh
+++ bin/install-arcion.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /opt/stage/arcion/replicant-cli/bin/replicant ]; then
   sudo mkdir -p /opt/stage/arcion; chown "$(logname)" /opt/stage/arcion
-  cd /opt/stage/arcion && curl -O --location https://arcion-releases.s3.us-west-1.amazonaws.com/general/replicant/replicant-cli-23.09.29.11.zip
+  cd /opt/stage/arcion && curl -O --location https://arcion-releases.s3.us-west-1.amazonaws.com/general/replicant/replicant-cli-24.01.25.1.zip
   unzip -q replicant-cli-*.zip
   rm replicant-cli-*.zip
   echo "arcion  /opt/stage/arcion/replicant-cli/bin/replicant downloaded"
--- bin/install-jdk.sh
+++ bin/install-jdk.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# of osx
 brew install openjdk@8  # can't install on ARM64
 
 brew install openjdk@17  # can't install on ARM64
+
+sudo apt-get install -y openjdk-17-jre-headless  # version 17.0.2+8-1
