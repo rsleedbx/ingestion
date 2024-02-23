d0f832efdf038d55054c03f3ead1bf11643a104e
robert-s-lee <sangkyulee@gmail.com>
2024-02-23 15:08:55 -0500

    readme

--- demo/sqlserver/run-ycsb-sqlserver-source.sh
+++ demo/sqlserver/run-ycsb-sqlserver-source.sh
@@ -975,7 +975,7 @@ start_arcion() {
   # run arcion
   set -x 
   cd $ARCION_CFG_DIR; JAVA_HOME="$ARCION_JAVA_HOME" \
-  REPLICANT_MEMORY_PERCENTAGE=25 \
+  REPLICANT_MEMORY_PERCENTAGE=25.0 \
   JAVA_OPTS='"-Djava.security.egd=file:/dev/urandom" "-Doracle.jdbc.javaNetNio=false" "-XX:-UseCompressedOops"' \
   $ARCION_HOME/bin/$ARCION_BIN "${a_repltype}" \
                 ${ARCION_CFG_DIR}/src.yaml \
