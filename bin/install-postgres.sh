cd05198635da80aa1a408df3a38c31e1e506ac55
Robert Lee <sangkyulee@gmail.com>
2024-03-15 12:01:45 -0400

    add postgres setup

--- bin/install-postgres.sh
+++ bin/install-postgres.sh
@@ -1,54 +1,89 @@
 #!/usr/bin/env bash
 
-# install pg with wal2json plugin
+LOGNAME=$(logname 2>/dev/null)
+LOGNAME=${LOGNAME:-root}
 
-[ -z "$(dpkg -l postgresql 2>/dev/null)" ] && sudo apt-get -y update && sudo apt-get -y install postgresql 
+start_pg() {
+    local OS_TYPE=${1:-${OS_TYPE:-$(uname -o)}}
+    case "${OS_TYPE,,}" in
+    darwin)
+        pg_ctl restart --pgdata=/opt/homebrew/var/postgresql@14 --options "-c wal_level=logical -c max_replication_slots=10 -c max_connections=300 -c shared_buffers=80MB -c max_wal_size=3GB"
+        ;;
+    linux)
+        sudo pg_ctlcluster $(dpkg-query --showformat='${Version}' --show postgresql | cut -d+ -f1) main restart --options "-c wal_level=logical -c max_replication_slots=10 -c max_connections=300 -c shared_buffers=80MB -c max_wal_size=3GB"
+        ;;
+    *)
+        echo "not supported"
+        ;;
+    esac
+}
 
-sudo apt-get -y install postgresql-$(dpkg-query --showformat='${Version}' --show postgresql | cut -d+ -f1)-wal2json postgresql-contrib dialog
+stop_pg() {
+    local OS_TYPE=${1:-${OS_TYPE:-$(uname -o)}}
+    case "${OS_TYPE,,}" in
+    darwin)
+        pg_ctl stop --pgdata=/opt/homebrew/var/postgresql@14 
+        ;;
+    linux)
+        sudo pg_ctlcluster stop 
+        ;;
+    *)
+        echo "not supported"
+        ;;
+    esac
+}
 
-# start pg with cdc and demo performance setup     
-sudo pg_ctlcluster $(dpkg-query --showformat='${Version}' --show postgresql | cut -d+ -f1) main restart --options "-c wal_level=logical -c max_replication_slots=10 -c max_connections=300 -c shared_buffers=80MB -c max_wal_size=3GB"
+install_pg() {
+    local OS_TYPE=${1:-${OS_TYPE:-$(uname -o)}}
+    case "${OS_TYPE,,}" in
+    darwin)
+        brew list postgresql@14 || brew install postgresql@14
+        brew list wal2json || brew install wal2json 
+        createuser --superuser postgres
+        ;;
+    linux)
+        # install pg with wal2json plugin
+        [ -z "$(dpkg -l postgresql 2>/dev/null)" ] && sudo apt-get -y update && sudo apt-get -y install postgresql 
 
-# create arcsrc user with default database
-# set synchronous_commit TO off for performance demo that recommeded for production usage
-# allow replication prov
-sudo -u postgres psql <<EOF
-CREATE USER arcsrc PASSWORD 'Passw0rd';
-create database arcsrc;
-ALTER DATABASE arcsrc SET synchronous_commit TO off;
-alter user arcsrc replication;
-alter database arcsrc owner to arcsrc;
-grant all privileges on database arcsrc to arcsrc;
-EOF
-# MAGIC
-# create replication slot arcsrc_w2j and heartbeat that will be used by arcion
-export PGPASSWORD=Passw0rd 
-psql --username arcsrc --dbname arcsrc --host 127.0.0.1 <<EOF
-SELECT 'init' FROM pg_create_logical_replication_slot('arcsrc_w2j', 'wal2json');
-SELECT * from pg_replication_slots;
-CREATE TABLE IF NOT EXISTS "REPLICATE_IO_CDC_HEARTBEAT"(
-    TIMESTAMP BIGINT NOT NULL,
-    PRIMARY KEY(TIMESTAMP)
-);  
-EOF
+        sudo apt-get -y install postgresql-$(dpkg-query --showformat='${Version}' --show postgresql | cut -d+ -f1)-wal2json postgresql-contrib dialog
+        ;;
+    *)
+        echo "not supported"
+        ;;
+    esac
+}
 
+pg_cli() {
+    local OS_TYPE=${1:-${OS_TYPE:-$(uname -o)}}
+    local SRC_DB=${SRC_DB:-${LOGNAME:-arcsrc}}
+    local SRCDB_HOST=${SRCDB_HOST:-localhost}
+    local SRCDB_PORT=${SRCDB_PORT:-5432}
+    local SRCDB_ARC_USER=${SRCDB_ARC_USER:-arcsrc}
+    local SRCDB_ARC_PW=${SRCDB_ARC_PW:-Passw0rd}
+    # when stdin is redirected
+    export PGPASSWORD=${SRCDB_ARC_PW} 
+    if [ ! -t 0 ]; then
+        local pg_cli_batch_mode="--csv"
+        cat <(printf "\n") - | \
+        psql -d "$SRCDB_DB" -h "$SRCDB_HOST" -p "$SRCDB_PORT" -U "${SRCDB_ARC_USER}" $pg_cli_batch_mode "$@"
+    else
+        psql -d "$SRCDB_DB" -h "$SRCDB_HOST" -p "$SRCDB_PORT" -U "${SRCDB_ARC_USER}" $pg_cli_batch_mode "$@"
+    fi
+}
 
-# create YCSB table
-export PGPASSWORD=Passw0rd 
-psql --username arcsrc --dbname arcsrc --host 127.0.0.1 <<EOF
-CREATE TABLE IF NOT EXISTS usertable (
-    YCSB_KEY INT PRIMARY KEY,
-    FIELD0 TEXT, FIELD1 TEXT,
-    FIELD2 TEXT, FIELD3 TEXT,
-    FIELD4 TEXT, FIELD5 TEXT,
-    FIELD6 TEXT, FIELD7 TEXT,
-    FIELD8 TEXT, FIELD9 TEXT
-); 
-EOF
-
-# show tables
-export PGPASSWORD=Passw0rd 
-psql --username arcsrc --dbname arcsrc --host 127.0.0.1 <<EOF
--- show tables
-\dt
-EOF
+pg_root_cli() {
+    local OS_TYPE=${1:-${OS_TYPE:-$(uname -o)}}
+    local SRC_DB=${SRC_DB:-${LOGNAME:-arcsrc}}
+    local SRCDB_HOST=${SRCDB_HOST:-localhost}
+    local SRCDB_PORT=${SRCDB_PORT:-5432}
+    local SRCDB_ROOT_USER=${SRCDB_ROOT_USER:-postgres}
+    local SRCDB_ROOT_PW=${SRCDB_ARC_PW:-Passw0rd}
+    # when stdin is redirected
+    if [ ! -t 0 ]; then
+        local pg_cli_batch_mode="--csv"
+        cat <(printf "\n") - | \
+        psql -d "$SRCDB_DB" -h "$SRCDB_HOST" -p "$SRCDB_PORT" -U "${SRCDB_ROOT_USER}" $pg_cli_batch_mode "$@"
+    else
+        psql -d "$SRCDB_DB" -h "$SRCDB_HOST" -p "$SRCDB_PORT" -U "${SRCDB_ROOT_USER}" $pg_cli_batch_mode "$@"
+    fi
+}
