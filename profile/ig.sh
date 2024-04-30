d904d57d5af1884543d049a28f89f3ba1d3614d3
Robert Lee <sangkyulee@gmail.com>
2024-04-30 11:15:10 -0400

    fix config_file

--- profile/ig.sh
+++ profile/ig.sh
@@ -3,7 +3,7 @@ export ARCION_DOWNLOAD=""       # don't donwload.  use what is specified below
 export ARCION_BASEDIR=/arcion   # where arcion is setup with IG
 export ARCION_DIRNAME=          # no separate subdir with IG
 export ARCION_HOME=/arcion
-export ARCION_BIN=${ARCION_HOME/bin/replicant
+export ARCION_BIN=${ARCION_HOME}/bin/replicant
 
 export SRCDB_ARC_USER=${SRCDB_ARC_USER:-arcsrc}
 export SRCDB_ARC_PW=${SRCDB_ARC_PW:-Passw0rd}
