1a14290a85388f12f76768b9d0ec380631f8d41c
Robert Lee <sangkyulee@gmail.com>
2024-05-15 15:21:11 -0400

    fix root

--- bin/install-sqlserver.sh
+++ bin/install-sqlserver.sh
@@ -58,8 +58,8 @@ sql_root_cli() {
     local DB_DB=${DB_DB:-master}
     local DB_HOST=${DB_HOST:-localhost}
     local DB_PORT=${DB_PORT:-1433}
-    local DB_ROOT_USER=${DB_ARC_USER:-sa}
-    local DB_ROOT_USER=${DB_ARC_PW:-Passw0rd}
+    local DB_ROOT_USER=${DB_ROOT_USER:-sa}
+    local DB_ROOT_PW=${DB_ROOT_PW:-Passw0rd}
     
   if [ -z "$(command -v sqlcmd)" ]; then export PATH=/opt/mssql-tools18/bin:$PATH; fi
   # when stdin is redirected
